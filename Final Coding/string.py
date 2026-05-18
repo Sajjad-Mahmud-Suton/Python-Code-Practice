4475997437c13261b7246a828287152036dafec9
MD. SAJJAD MAHMUD SUTON <sajjadmahmudsuton@gmail.com>
2026-05-18 14:14:00 +0600

    String

---
+++ Final Coding/string.py
@@ -0,0 +1,24 @@
+str1 = "I'm Sajjad"
+str2 = 'I am "Sajjad"'
+print(str1)
+print(str2)
+
+#Multiple line printing
+str3='''
+Hey 
+i am
+sajjad
+'''
+print(str3)
+
+#printing by index
+Name = "Sajjad"
+print(Name[0])
+print(Name[1])
+print (Name[-1]) #negative index for last value
+
+#from any index to another index 0 to 3
+print(Name[0:3])
+
+#print index marked to last
+print(Name[2:])
