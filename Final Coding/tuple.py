23c53f41d0167e7fda594d87db86e3e731dc2394
MD. SAJJAD MAHMUD SUTON <sajjadmahmudsuton@gmail.com>
2026-05-19 01:04:32 +0600

    Enhance tuple and unpacking examples with additional print statements and operations

--- Final Coding/tuple.py
+++ Final Coding/tuple.py
@@ -1,3 +1,9 @@
 num = (5,2,6,7,2)
 print(num)
 print(num[3])
+
+num1 = (3,5,5,2,3)
+num2 = num + num1
+print(num2)
+
+print(num1*2)
--- Final Coding/unpacking.py
+++ Final Coding/unpacking.py
@@ -7,3 +7,12 @@ print(y)
 print(z)
 
 print(x,y,z)
+
+num = (3,6,7,3,23,4)
+print(num)
+
+x,y,*z = num
+print(x,y,z)
+
+x,*y,z = num
+print(x,y,z)
