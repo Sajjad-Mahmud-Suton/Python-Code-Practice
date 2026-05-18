b1df1802a722519d62f270ccf6e4a90f1376d065
MD. SAJJAD MAHMUD SUTON <sajjadmahmudsuton@gmail.com>
2026-05-18 13:59:39 +0600

    practice python

---
+++ Final Coding/first.py
@@ -0,0 +1,7 @@
+print("Sajjad")
+print("*"*10)
+
+
+name= "Sajjad"
+age = 20
+print("My name is "+name+ " and I am " + str(age) + " years old")
---
+++ Final Coding/taking_input.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+print("Hi " + name)
+
+color = input("What is your favourite color? ")
+print(name + " likes " + color)
+
+pounds= int(input("Enter Your Weight: "))
+kg = pounds/2.205
+print("Your weight in kg: " , round(kg,2))
